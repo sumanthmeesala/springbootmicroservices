72c1228a5f972e55144799c9ef8a83ed9d757552
Sumanth <sumanth@gmail.com>
2024-09-09 13:49:30 +0530

    Initial Commit

---
+++ springbootmicroservices/DiaryService/src/main/java/sumu/project/DiaryService/DiaryServiceApplication.java
@@ -0,0 +1,13 @@
+package sumu.project.DiaryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryServiceApplication.class, args);
+	}
+
+}
---
+++ springbootmicroservices/DiaryService/src/test/java/sumu/project/DiaryService/DiaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sumu.project.DiaryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
